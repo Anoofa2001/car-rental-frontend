6c736bc3eeb7b28571d8e3f0a280283fc90a96ba
Aysha Anoofa <aysha.anoofa@segnitos.in>
2026-02-17 01:59:18 +0530

    added folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import React, { useState} from 'react'
-import Navbar from './components/navbar'
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './pages/Home';
-import CarDetails from './pages/CarDetails';
-import Cars from './pages/Cars';
-import MyBookings from './pages/MyBookings';
+import React, { useState } from "react";
+import Navbar from "./components/navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import CarDetails from "./pages/CarDetails";
+import Cars from "./pages/Cars";
+import MyBookings from "./pages/MyBookings";
+import Layout from "./pages/Owner/Layout";
+import AddCar from "./pages/Owner/AddCar";
+import ManageCars from "./pages/Owner/ManageCars";
+import ManageBookings from "./pages/Owner/ManageBookings";
 
 const App = () => {
-
   const [showLogin, setShowLogin] = useState(false);
-  const isOwnerPath = useLocation().pathname.startsWith('/owner');
-  
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
+
   return (
     <>
-      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cars-details/:id" element={<CarDetails />} />
         <Route path="/cars" element={<Cars />} />
-        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Routes path="/owner/*" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-car" element={<AddCar />} />
+          <Route path="manage-cars" element={<ManageCars />} />
+          <Route path="manage-bookings" element={<ManageBookings />} />
+        </Routes>
       </Routes>
-
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Owner/NavbarOwner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { assets, dummyCarData } from "../../assets/assets";
+import { Link } from "react-router-dom";
+
+const NavbarOwner = () => {
+  const user = dummyCarData;
+  return (
+    <div className="flex items-center justify-between px-6 md:px-10 py-4 text-gray-500 border-b border border-borderColor relative transition-all">
+        <Link>
+        <img src={assets.logo} alt="" className="h-7" />
+        <p>Welcome, {user.name || "Owner"}</p>
+        </Link>
+    </div>
+  );
+};
+
+export default NavbarOwner;
